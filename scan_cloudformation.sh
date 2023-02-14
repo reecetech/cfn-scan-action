181f537fe8ed83649aa36bd5d4d2df6103f71dc0
Alex Hou <alex.hou@reece.com.au>
2023-02-14 16:32:33 +1100

    Pass parameters as env vars and fix bash var references

--- scan_cloudformation.sh
+++ scan_cloudformation.sh
@@ -2,9 +2,10 @@
 
 set -euo pipefail
 
-contents=$(cat "${{ inputs.file-path }}" jq ',' -MRs)  # MODIFIED: 'cat' instead of '<' (faster)
-payload="{\"data\":{\"attributes\":{\"type\":\"cloudformation-template\",\"accountId\":\"${{ inputs.account-id }}\",\"contents\":${contents}}}}"
-# Should account ID be hard-coded? This assumes that it should not be
+contents=$(cat "${FILE_PATH}" jq ',' -MRs)
+
+# TO DO: Add if statement checking if CC_ACCOUNT_ID is empty; allows empty
+payload="{\"data\":{\"attributes\":{\"type\":\"cloudformation-template\",\"accountId\":\"${CC_ACCOUNT_ID}\",\"contents\":${contents}}}}"
 
 echo ""
 echo "INFO: Beginning security scan of template"
@@ -13,9 +14,9 @@ echo Request:
 echo "${payload}" | jq '.' -MR  # Without -R flag is throwing errors (control characters need to be escaped)
 
 response=$(set +e ; curl -L --fail --show-error --silent -X POST \
-    -H "Authorization: ApiKey ${api_key}" \
+    -H "Authorization: ApiKey ${CC_API_KEY}" \
     -H "Content-Type: application/vnd.api+json" \
-    https://${{ inputs.aws-region }}-api.cloudconformity.com/v1/iac-scanning/scan \
+    https://${AWS_REGION}-api.cloudconformity.com/v1/iac-scanning/scan \
     --data-binary "${payload}" | jq '.' -M | jq '.data[] | select(.attributes.status == "FAILURE")' ; set -e)
 
 if [ -n "$response" ]; then
